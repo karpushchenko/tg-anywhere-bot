12881bc6f45fd1d4570fb570e7d0844cf57b9952
Ivan Karpushchenko <karpushchenko@mail.ru>
2023-01-26 19:59:44 +0400

    Settings saving animation

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Script from 'next/script';
 import { useState } from 'react';
 import axios from 'axios';
@@ -10,12 +9,18 @@ const Placeholder = ({children} : { children: string}) => {
   </b>);
 }
 
+const Loader = () => <svg className="animate-spin ml-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+<path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+</svg>
+
 export default function Settings() {
   const [webhook, setWebhook] = useState('');
   const [schema, setSchema] = useState('');
   const [loaded, setLoaded] = useState(false);
+  const [saving, setSaving] = useState(false);
   const [userId, setUserId] = useState(undefined);
-  const getWebhook = async (userId) => {
+  const getWebhook = async (userId:number) => {
     // const model = new Model;
     const user = {};
 
@@ -34,7 +39,8 @@ export default function Settings() {
       })
 
   }
-  const saveData = async (event) => {
+  const saveData = async (event: React.SyntheticEvent) => {
+    setSaving(true);
     event.preventDefault();
     axios.post('/api/user', {
       id : userId,
@@ -42,6 +48,8 @@ export default function Settings() {
         webhook: webhook,
         schema: schema,
       }
+    }).then( () => {
+      setSaving(false);
     });
   }
   return (
@@ -120,9 +128,10 @@ export default function Settings() {
                         <button
                           type="submit"
                           className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                          Save
+                          >
+                          Save {saving && <Loader/>}
                         </button>
+                          
                       </div>
                     </div>
                   </form>
